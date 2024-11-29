02421686d22ae1af50fa0375099b0b16e952c99e
Pedro Lucas Barbosa Costa <pedrolucasbcosta@gmail.com>
2024-11-29 16:49:51 -0300

    configurações

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOURCE_DIR="$HOME/documentos"
+
+BACKUP_DIR="$HOME/backups"
+
+LOG_FILE="$BACKUP_DIR/logs/backup.log"
+
+
+ENABLE_RETENTION=true
+RETENTION_DAYS=7  #quantos dias manter os backups: opcional, mude o numero
